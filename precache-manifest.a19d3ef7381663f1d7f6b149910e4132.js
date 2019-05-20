c4edd2dc838de4a14391f8223058d8bbac96a6ef
unxplainable <43263583+unxplainable@users.noreply.github.com>
2019-05-20 11:21:32 +0700

    Updates

---
+++ precache-manifest.a19d3ef7381663f1d7f6b149910e4132.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08113af183c336873bfe2ed19b92f418",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "981598700a8e442e70cb",
+    "url": "/home/static/css/main.6f19a7e4.chunk.css"
+  },
+  {
+    "revision": "7dafa317a8587fd4913a",
+    "url": "/home/static/js/2.67d6449f.chunk.js"
+  },
+  {
+    "revision": "981598700a8e442e70cb",
+    "url": "/home/static/js/main.6d3dac39.chunk.js"
+  },
+  {
+    "revision": "4e1ec8947a6857600002",
+    "url": "/home/static/js/runtime~main.0990ee84.js"
+  },
+  {
+    "revision": "e7d1a1fb5cbdd2718eec49690c29cce9",
+    "url": "/home/static/media/background-1.e7d1a1fb.jpg"
+  },
+  {
+    "revision": "615c6e1081e8d67e20b03676169dcdc2",
+    "url": "/home/static/media/profile-img.615c6e10.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.2ef6a6ae0ad9cfcf41d128d27478dfcb.js"
+  "/home/precache-manifest.a19d3ef7381663f1d7f6b149910e4132.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
